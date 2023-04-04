f5578b9b1c79afc2b150fd4e46f9a9dcdbaa0cb3
rainofsilence <rainofsilence@foxmail.com>
2023-04-05 03:00:36 +0800

    初始化

---
+++ spmdv2-web/src/main/java/cn/silence/Application.java
@@ -0,0 +1,19 @@
+package cn.silence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <em>Application</em>
+ *
+ * @author rainofsilence
+ * @date 2023/04/05 2:51:51
+ * @desc
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
